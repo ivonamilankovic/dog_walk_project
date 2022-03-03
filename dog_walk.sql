7647955c9925ce962e59427595ebe3b5eec39136
SaraBabic <sarababic0101@gmail.com>
2022-03-03 23:49:17 +0100

    database

---
+++ dog_walk.sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 03, 2022 at 11:46 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dog_walk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notebook`
+--
+
+CREATE TABLE `notebook` (
+  `id_notebook` int(8) NOT NULL,
+  `id_walker` int(8) NOT NULL,
+  `id_user` int(8) NOT NULL,
+  `id_walk` int(8) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `duration` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `id_reservation` int(8) NOT NULL,
+  `id_walker` int(8) NOT NULL,
+  `id_user` int(8) NOT NULL,
+  `date` date NOT NULL,
+  `time` datetime NOT NULL,
+  `location` varchar(40) NOT NULL,
+  `dog_breed` varchar(30) NOT NULL,
+  `dog_name` varchar(20) NOT NULL,
+  `dog_gender` char(1) NOT NULL,
+  `age` tinyint(2) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `specifics` varchar(255) NOT NULL,
+  `finished` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(8) NOT NULL,
+  `role` char(1) NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `verify` tinyint(1) DEFAULT NULL,
+  `phone_number` varchar(20) NOT NULL,
+  `address` varchar(40) NOT NULL,
+  `picture` varchar(40) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT NULL,
+  `rate` tinyint(10) DEFAULT NULL,
+  `number_of_walks` int(255) DEFAULT NULL,
+  `dog_breeds` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `notebook`
+--
+ALTER TABLE `notebook`
+  ADD PRIMARY KEY (`id_notebook`);
+
+--
+-- Indexes for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`id_reservation`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `notebook`
+--
+ALTER TABLE `notebook`
+  MODIFY `id_notebook` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `id_reservation` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(8) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
